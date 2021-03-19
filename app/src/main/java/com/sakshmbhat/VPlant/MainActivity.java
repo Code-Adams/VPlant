fea6aa4dbb3bb8f0fa87db7484f060d129202d6a
Adarsh Shukla <50777364+ideallyadarsh@users.noreply.github.com>
2021-03-20 02:09:41 +0530

    Navigation Drawer & Fragments:

--- app/src/main/java/com/sakshmbhat/VPlant/MainActivity.java
+++ app/src/main/java/com/sakshmbhat/VPlant/MainActivity.java
@@ -1,14 +1,77 @@
 package com.sakshmbhat.VPlant;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;//navigation drawer
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //naviagtion drawer
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drawer=findViewById(R.id.drawer_layout);
+        NavigationView navigationView =findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState==null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new PlantCatalog()).commit();
+            navigationView.setCheckedItem(R.id.nav_plants);
+
+        }
     }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_plants:
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new PlantCatalog()).commit();
+            break;
+            case R.id.nav_maintain:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Tips_Info()).commit();
+                break;
+            case R.id.nav_contact:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ContactFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+   public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        super.onBackPressed();
+   }
+
+
 }
