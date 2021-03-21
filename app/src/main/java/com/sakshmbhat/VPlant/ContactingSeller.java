7e1dc864358f2dcedadbb4885772b303fdff3c84
sakshm bhat <sakshm2000@gmail.com>
2021-03-21 10:26:21 +0530

    committing un committed file

---
+++ app/src/main/java/com/sakshmbhat/VPlant/ContactingSeller.java
@@ -0,0 +1,14 @@
+package com.sakshmbhat.VPlant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactingSeller extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacting_seller);
+    }
+}
